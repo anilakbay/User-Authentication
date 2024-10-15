1c37579262ac9ba3b5f6e8f81076dcf73f16124a
anil_akbay <anilakbay20@gmail.com>
2024-10-15 23:30:56 +0300

    klasör ve dosyalar oluşturuluyor

--- src/config.js
+++ src/config.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const pasth = require("path");
+const bcrypt = require("bcrypt");
+
+const app = express();
+
+// use ejs as view engine
+app.set("view engine", "ejs");
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server running on Port ${port}`);
+});
