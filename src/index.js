1b0f7095585f0212c4a2d078bdb2c28fad860aca
anil_akbay <anilakbay20@gmail.com>
2024-10-16 03:14:27 +0300

    kodlar yazılıyor ve eksikler tamamlanıyor!

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const bcrypt = require("bcrypt");
 const collection = require("./config");
 
 const app = express();
+//convert data into json format
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
 
 // use ejs as view engine
 app.set("view engine", "ejs");
@@ -26,11 +30,41 @@ app.post("/signup", async (req, res) => {
     password: req.body.password,
   };
 
-  const userdata = await collection.insertMany(data);
-  console.log(userdata);
+  const existingUser = await collection.findOne({ name: data.name });
+  if (existingUser) {
+    res.send("User already exists. Please choose a different username.");
+  } else {
+    // hash the password using bcrypt
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+    data.password = hashedPassword; // Replace hash the password with original password
+
+    const userdata = await collection.insertMany(data);
+    console.log(userdata);
+  }
 });
+
+// Login user
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.name });
+        if(!check){
+            res.send("user name cannot found");
+        }
+
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if(isPasswordMatch){
+            res.send("home");
+        }else{
+            res.send("wrong password");
+        }
+    } catch {
+        res.send ("wrong Details");
+    }
+}
 
 const port = 5000;
 app.listen(port, () => {
-  console.log(`Server running on Port ${port}`);
+  console.log(`Server running on Port: ${port}`);
 });
